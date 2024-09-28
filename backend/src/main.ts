5c00f5b90ac4947f12378a5855ef2367999e0a5d
Adrian Maj <adrianmaj1122@gmail.com>
2024-09-28 13:41:40 +0200

    Fixed CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
