20c94a2985a4db036bc3163f12ae0f469d23e369
kokodrillo03 <krzychu.biska2@gmail.com>
2024-09-28 20:12:24 +0200

    lellloeoeo

--- backend/src/accidents/accidents.module.ts
+++ backend/src/accidents/accidents.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccidentEntity, AccidentSchema } from './entities/accident.entity';
-import { AccidentIndexerService } from './accidentIndexer.service';
+import { AccidentsService } from './accidents.service';
 import { HttpModule } from '@nestjs/axios';
+import { AccidentsController } from './accidents.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: AccidentEntity.name, schema: AccidentSchema }]),
     HttpModule
   ],
-  providers: [AccidentIndexerService],
+  providers: [AccidentsService],
+  controllers: [AccidentsController],
 })
-export class AccidentIndexerModule {}
+export class AccidentsModule {}
