b996ae7adc3ed5c13e51bc9131f010bcff88c62f
Adrian Maj <adrianmaj1122@gmail.com>
2024-09-29 00:52:49 +0200

    Fixed button styling and conditions

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -3,13 +3,9 @@
     <h1 class="text-3xl font-bold">Moje konto</h1>
     <div class="flex items-center gap-3 text-lg">
       <div class="h-12 w-12 rounded-full bg-slate-500"></div>
-      <span>{{
-        authStore.user && authStore.isLoggedIn
-          ? `${authStore.user?.email}`
-          : 'Niezalogowany użytkownik'
-      }}</span>
+      <span>{{ authStore.user ? `${authStore.user?.email}` : 'Niezalogowany użytkownik' }}</span>
     </div>
-    <div class="mt-auto" v-if="authStore.user && authStore.isLoggedIn">
+    <div class="mt-auto flex flex-col gap-4" v-if="!authStore.user">
       <RouterLink class="w-full" to="/login">
         <BaseButton class="w-full">Zaloguj się</BaseButton>
       </RouterLink>
