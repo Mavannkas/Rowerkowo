42e871cdcdaf7ed210b97f8a930eef087cdbef91
Kamil-Budzik <mixboksu@gmail.com>
2024-09-29 05:17:03 +0200

    Translations

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -14,11 +14,11 @@
       <div class="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
         <div class="space-y-4 p-6 sm:p-8 md:space-y-6">
           <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-            Sign in to your account
+            Zarejestruj się
           </h1>
           <form class="space-y-4 md:space-y-6" @submit.prevent="handleLogin">
             <BaseFormInput
-              label="Your email"
+              label="Twój email"
               type="email"
               name="email"
               id="email"
@@ -29,7 +29,7 @@
               required
             />
             <BaseFormInput
-              label="Password"
+              label="Hasło"
               type="password"
               v-model="password"
               id="password"
@@ -42,11 +42,11 @@
               v-if="loginMutation.isError.value"
               class="mt-1 text-sm text-red-600 dark:text-red-500"
             >
-              Invalid email or password
+              Nieprawidłowy email lub hasło!
             </p>
             <div class="flex items-center justify-between">
               <a href="#" class="text-sm font-medium text-primary-600 hover:underline"
-                >Forgot password?</a
+                >Zapomniałeś hasła?</a
               >
             </div>
             <BaseSpinner v-if="loginMutation.isPending.value" />
@@ -55,14 +55,14 @@
               type="submit"
               class="w-full bg-primary-600 px-5 py-2.5 hover:bg-primary-700"
             >
-              Sign in
+              Zaloguj się
             </BaseButton>
             <p class="text-sm font-light text-gray-500">
-              Don’t have an account yet?
+              Nie masz jeszcze konta?
               <RouterLink
                 :to="ROUTING_URLS.REGISTER"
                 class="font-medium text-primary-600 hover:underline"
-                >Sign up</RouterLink
+                >Zarejestruj się</RouterLink
               >
             </p>
           </form>
@@ -105,10 +105,10 @@ const loginMutation = useMutation({
       email.value = ''
     } else if (axios.isAxiosError(error)) {
       console.log(
-        `An unknown error occured: ${error.response?.status}, Message: ${error.response?.data.message}`
+        `Wystąpił nieznany błąd: ${error.response?.status}, Wiadomość: ${error.response?.data.message}`
       )
     } else {
-      console.log(`An unknown error occured: ${error.name}, Message: ${error.message}`)
+      console.log(`Wystąpił nieznany błąd: ${error.name}, Wiadomość: ${error.message}`)
     }
   },
   onSuccess: (data) => {
